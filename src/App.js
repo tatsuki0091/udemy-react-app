760047fd5e99950ba126c4429302af15febba191
“tatsuki0091” <rossosalvatore@icloud.com>
2020-01-08 20:21:56 +0900

    propsについて

--- src/App.js
+++ src/App.js
@@ -13,15 +13,28 @@ import React from 'react';
 // }
 
 const App = () => {
-  return <div>
-    <Cat />
-    <Cat />
-    <Cat />
-    <Cat />
-  </div>
+  const profiles = [
+    {name: "Taro", age:10},
+    {name: "Hanako", age:5},
+    {name: "Noname"}
+  ]
+  return (
+  <div>
+    {
+      profiles.map((profile, index) => {
+        return <User name={profile.name} age={profile.age} key={index}/>
+      })
+    }
+  </div>)
 }
 
-const Cat = () => {
-  return <div>myao</div>
+const User = (props) => {
+  return <div>
+    Hi! I am {props.name} and {props.age} years old.
+    </div>
+}
+
+User.defaultProps = {
+  age: 1
 }
 export default App;
