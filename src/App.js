938b969be631a95eb58dc0e3d3fc0bfb3340e863
“tatsuki0091” <rossosalvatore@icloud.com>
2020-01-08 19:53:22 +0900

    componentについて

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
 
 
-function App() {
-  const greeting = "hi tom";
-  const dom = <h1 className="foo">{greeting}</h1>;
-  return (
-    <React.Fragment>
-      <label htmlForm="bar">bar</label>
-      <input type="text" onChange={() => {console.log("I am clicked")}} />
-    </React.Fragment>
-  );
+// function App() {
+//   const greeting = "hi tom";
+//   const dom = <h1 className="foo">{greeting}</h1>;
+//   return (
+//     <React.Fragment>
+//       <label htmlForm="bar">bar</label>
+//       <input type="text" onChange={() => {console.log("I am clicked")}} />
+//     </React.Fragment>
+//   );
+// }
+
+const App = () => {
+  return <div>
+    <Cat />
+    <Cat />
+    <Cat />
+    <Cat />
+  </div>
 }
 
+const Cat = () => {
+  return <div>myao</div>
+}
 export default App;
