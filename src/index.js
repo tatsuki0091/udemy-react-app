52a32fdcc0e3b79bc347437951fa20b823efaa83
“tatsuki0091” <rossosalvatore@icloud.com>
2020-01-13 15:35:35 +0900

    一覧表示

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import './index.css';
-import reducer from './reducers'
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
+import './index.css';
+import reducer from './reducers';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
 
 const store = createStore(reducer)
+
 ReactDOM.render(
     <Provider store={store}>
         <App />
-    </Provider>
-, document.getElementById('root')
+    </Provider>,
+     document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
