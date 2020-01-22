907e6a0bac2364b0818482a46eef3f3fdadc031e
“tatsuki0091” <rossosalvatore@icloud.com>
2020-01-22 20:23:01 +0900

    event_newコンポーネント作成

--- src/components/events_new.js
+++ src/components/events_new.js
@@ -1,19 +1,61 @@
 import React, {Component} from 'react';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
+import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
 
-class EventsNew extends Component {    
-  render() {
+import { postEvent } from '../actions'
+
+class EventsNew extends Component { 
+  constructor(props) {
+    super(props)
+    this.onSubmit = this.onSubmit.bind(this)
+  }
+
+  renderField(field) {
+    const {input, label, type, meta: { touched, error } } = field
+
     return (
-      <React.Fragment>
-          <div>foo</div>
+    <div>
+      <input {...input} placeholder={label} type={type} />
+      {touched && error && <span>{error}</span>}
 
-      </React.Fragment>
+    </div>
+    )
+  }
+
+  async onSubmit(values) {
+    await this.props.postEvent(values)
+    this.props.history.push('/')
+  } 
+
+
+  render() {
+      const { handleSubmit } = this.props
+      return (
+        <form onSubmit={handleSubmit(this.onSubmit)}>
+          <div><Field label="Title" name="title" type="text" component={this.renderField} /></div>
+          <div><Field label="Body" name="body" type="text" component={this.renderField} /></div>
+          <div>
+            <input type="submit" value="Submit" disable={false} />
+            <Link to="/">Cancel</Link>
+          </div>
+        </form>
     )
   }
 }
 
-// const mapDispatchToProps = ({
-//   postEvent
-// })
+const validate = values => {
+  const errors = {}
 
-export default connect(null, null)(EventsNew)
+  if(!values.title) errors.title = "Enter a title"
+  if(!values.body) errors.body = "Enter a body"
+
+  return errors
+}
+const mapDispatchToProps = ({
+  postEvent
+})
+
+export default connect(null, mapDispatchToProps)(
+  reduxForm({ validate, form: 'eventNewForm' })(EventsNew)
+)
