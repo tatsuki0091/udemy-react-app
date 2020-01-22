52ade6a9a4a900f91225aa67b6e8a91b73c57b1a
“tatsuki0091” <rossosalvatore@icloud.com>
2020-01-22 23:40:14 +0900

    pristine,submitting作成

--- src/components/events_new.js
+++ src/components/events_new.js
@@ -30,13 +30,13 @@ class EventsNew extends Component {
 
 
   render() {
-      const { handleSubmit } = this.props
+      const { handleSubmit, pristine, submitting } = this.props
       return (
         <form onSubmit={handleSubmit(this.onSubmit)}>
           <div><Field label="Title" name="title" type="text" component={this.renderField} /></div>
           <div><Field label="Body" name="body" type="text" component={this.renderField} /></div>
           <div>
-            <input type="submit" value="Submit" disable={false} />
+            <input type="submit" value="Submit" disabled={pristine || submitting} />
             <Link to="/">Cancel</Link>
           </div>
         </form>
