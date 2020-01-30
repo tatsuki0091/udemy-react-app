b6af42435b2b196a06714db43096ef0d9c7db51f
“tatsuki0091” <rossosalvatore@icloud.com>
2020-01-30 19:58:26 +0900

    material-uitekiyou

--- src/components/events_new.js
+++ src/components/events_new.js
@@ -3,7 +3,12 @@ import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
 import { Link } from 'react-router-dom';
 
+import { Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn} from 'material-ui/Table'
+import FloatingActionButton from 'material-ui/FloatingActionButton'
+import ContentAdd from 'material-ui/svg-icons/content/add'
+import RaisedButton from 'material-ui/RaisedButton'
 import { postEvent } from '../actions'
+import { TextField } from 'material-ui';
 
 class EventsNew extends Component { 
   constructor(props) {
@@ -15,11 +20,13 @@ class EventsNew extends Component {
     const {input, label, type, meta: { touched, error } } = field
 
     return (
-    <div>
-      <input {...input} placeholder={label} type={type} />
-      {touched && error && <span>{error}</span>}
-
-    </div>
+    <TextField 
+    hintText={label} 
+    floatingLabelText={label}
+    type={type}
+    errorText={touched && error}
+    {...input}
+    fullWidth={true}/>
     )
   }
 
@@ -31,13 +38,15 @@ class EventsNew extends Component {
 
   render() {
       const { handleSubmit, pristine, submitting, invalid } = this.props
+      const style = {margin:12}
       return (
         <form onSubmit={handleSubmit(this.onSubmit)}>
+          
           <div><Field label="Title" name="title" type="text" component={this.renderField} /></div>
           <div><Field label="Body" name="body" type="text" component={this.renderField} /></div>
           <div>
-            <input type="submit" value="Submit" disabled={ pristine || submitting || invalid} />
-            <Link to="/">Cancel</Link>
+          <RaisedButton label="Submit" type="submit" style={style} disabled={ pristine || submitting || invalid} />
+          <RaisedButton label="Cancel" style={style} containerElement={<Link to="/"/>} />
           </div>
         </form>
     )
